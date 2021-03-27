ac84e99639b5b1f086a53d35207e50f5be7ceff1
student <kliment@andreev.it>
2021-03-27 17:02:54 +0000

    first commit

---
+++ src/main/java/com/caltech/cicspring/CicspringApplication.java
@@ -0,0 +1,13 @@
+package com.caltech.cicspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltech/cicspring/CicspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltech.cicspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
